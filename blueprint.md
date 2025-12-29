08ea49bd039be7fb3279345bd2640dd2ce3d4570
victoralfayo01 <victoralfayo01@gmail.com>
2025-12-29 09:02:03 +0000

    rename workspace to FUTURE_FS_01, added slugs and dynamic routes

---
+++ blueprint.md
@@ -0,0 +1,12 @@
+# FUTURE_FS_01
+
+## Overview
+
+This is a Next.js project.
+
+## Features
+
+* Next.js 16
+* React 19
+* TypeScript
+* Tailwind CSS
---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+export default async function Projects({ params }: { params: Promise<{projectId: string}>}){
+
+    const project_id = (await (params)).projectId;
+
+
+    return (
+    <>
+        <h1>Project {project_id}</h1>
+        <p><b>Tune in to view my projects</b></p>
+    </>
+    )
+}
